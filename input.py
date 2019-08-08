763ed51239e0c6ef989b760eead32f72d6250e7b
tgolowan <51421583+tgolowan@users.noreply.github.com>
2019-08-08 14:08:30 +0200

    Update input.py

--- input.py
+++ input.py
@@ -1,4 +1,4 @@
-api = input ("Please type yor API key:")
+api = input ("Please type your API key:")
 mac = input ("Please share your macaddress:")
 import requests
 r = requests.get('https://api.macaddress.io/v1?apiKey='+str(api)+'&output=vendor&search='+str(mac)+'')  
